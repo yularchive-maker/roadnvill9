27bdf9226e548dbe2416dab88991da2f60be1f17
yularchive-maker <yul.archive3@gmail.com>
2026-04-09 20:57:49 +0900

    Create page.js

---
+++ app/app/app/page.js
@@ -0,0 +1,14 @@
+import { redirect } from 'next/navigation'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
+import { cookies } from 'next/headers'
+
+export default async function Home() {
+  const supabase = createServerComponentClient({ cookies })
+  const { data: { session } } = await supabase.auth.getSession()
+
+  if (session) {
+    redirect('/dashboard')
+  } else {
+    redirect('/login')
+  }
+}
